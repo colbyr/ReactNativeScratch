ef290f9403594d1190c8f5f6275004d71d8ddb15
Colby Rabideau <colby.rabideau@robinpowered.com>
2020-11-06 16:42:40 -0500

    demo the blob registry leak

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,8 +23,24 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {registry} from 'react-native/Libraries/Blob/BlobRegistry';
 
 const App: () => React$Node = () => {
+  const [registeredBlobs, setRegisteredBlobs] = useState([]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(
+        `https://clients3.google.com/generate_204?cacheBreaker=${
+          Math.random() * 10000
+        }`,
+      ).then(() => {
+        const blobKeys = Object.keys(registry);
+        blobKeys.reverse();
+        setRegisteredBlobs(blobKeys);
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  });
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -32,39 +48,11 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
           <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+            <Text>
+              {registeredBlobs.length} registered blobs:{' '}
+              {JSON.stringify(registeredBlobs, null, 2)}
+            </Text>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -82,6 +70,7 @@ const styles = StyleSheet.create({
   },
   body: {
     backgroundColor: Colors.white,
+    padding: 10,
   },
   sectionContainer: {
     marginTop: 32,
